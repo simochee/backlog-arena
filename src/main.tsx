f77de544cf9a737d19580bea0f90eed617abaf6c
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-09-14 04:18:05 +0900

    refactor: remove fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./main.css";
-import "@fontsource/ibm-plex-sans-jp/japanese-400.css";
-import "@fontsource/ibm-plex-sans-jp/japanese-600.css";
 import { createHashHistory } from "@tanstack/history";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
